2cc1ce29c494d73c8f75bc24145b3f14afdd2886
Ram <ram.hamel@gmail.com>
2018-02-11 12:20:18 +0530

    Form Commit

---
+++ sample.java
@@ -0,0 +1,12 @@
+import statement
+class sample
+{
+public static void main(String arg[])throws IO expressions
+{
+int a;
+Scanner scanner=new Scanner();
+System.out.println("enter a value");
+a=Scanner.next Int();
+System.out.println("given value="+a);
+}
+}
